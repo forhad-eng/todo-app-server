5371794ffac2451146a64db5c180da360733029d
forhad-eng <forhadu.cse@gmail.com>
2022-05-19 02:57:43 +0600

    individual task delete api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const app = express()
@@ -9,15 +9,15 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-function verifyJWT(req, res, next){
+function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization
-    if(!authHeader){
-       return res.status(401).send({message: 'Unauthorized access'})
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' })
     }
     const token = authHeader.split(' ')[1]
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if(err){
-            return res.status(403).send({message: 'Forbidden access'})
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
         }
         req.decoded = decoded
         next()
@@ -48,7 +48,42 @@ async function run() {
             }
         })
 
-       
+        app.get('/task', verifyJWT, async (req, res) => {
+            const email = req.decoded.email
+            const result = await taskCollection.find({ email }).toArray()
+            res.send({ success: true, result })
+        })
+
+        app.post('/task', verifyJWT, async (req, res) => {
+            const task = req.body
+            const result = await taskCollection.insertOne(task)
+            if (result.insertedId) {
+                res.send({ success: true, message: `${task.taskName} is added successfully` })
+            }
+        })
+
+        app.patch('/task/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    complete: true
+                }
+            }
+            const result = await taskCollection.updateOne(filter, updatedDoc)
+            if (result.modifiedCount) {
+                res.send({ success: true, message: 'Congrats for completing!' })
+            }
+        })
+
+        app.delete('/task/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) }
+            const result = await taskCollection.deleteOne(filter)
+            if (result.deletedCount) {
+                res.send({ success: true, message: 'Deleted successfully!' })
+            }
+        })
     } finally {
     }
 }
