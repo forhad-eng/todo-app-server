4a9411b5e11973b5814300ab03869090bd96e70a
forhad-eng <forhadu.cse@gmail.com>
2022-05-19 01:01:58 +0600

    token generate and user insert into db api created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -15,6 +16,22 @@ async function run() {
     try {
         await client.connect()
         const taskCollection = client.db('todoDb').collection('tasks')
+        const userCollection = client.db('todoDb').collection('users')
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const filter = { email }
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: user
+            }
+            const accessToken = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            const result = await userCollection.updateOne(filter, updatedDoc, options)
+            if (result) {
+                res.send({ success: true, accessToken })
+            }
+        })
     } finally {
     }
 }
