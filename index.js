24c1b06ac1969fd304c44daab35a96ff13f1d2db
forhad-eng <forhadu.cse@gmail.com>
2022-05-18 23:10:28 +0600

    initial server setup done

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sacor.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+
+async function run() {
+    try {
+        await client.connect()
+        const taskCollection = client.db('todoDb').collection('tasks')
+    } finally {
+    }
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('Todo App server is running!')
+})
+
+app.listen(port, () => {
+    console.log('Listening to port', port)
+})
